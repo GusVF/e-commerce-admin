741ab93166bcc66ec5a586e59d3a9f91f243ff5f
Luiz Gustavo Ferreira <gus.116506@gmail.com>
2023-11-14 07:23:59 -0300

    Created Edit, Update, Delete of items @ api-list

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
@@ -80,7 +80,7 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({
       setLoading(true)
       await axios.delete(`/api/${params.storeId}/billboards/${params.billboardId}`);
       router.refresh();
-      router.push("/")
+      router.push(`/${params.storeId}/billboards`)
       toast.success("Billboard deleted.")
     } catch (error) {
       toast.error("Make sure to remove all categories using this billboard first!")
@@ -158,7 +158,6 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({
           </Button>
         </form>
       </Form>
-      <Separator />
     </>
   );
 };
