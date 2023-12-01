e5d2fdac062d537a6b2a666e2dc0021075ce568e
Luiz Gustavo Ferreira <gus.116506@gmail.com>
2023-12-01 09:39:28 -0300

    Updated package.json wiht new dependecies version

--- app/(dashboard)/[storeId]/layout.tsx
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -5,13 +5,13 @@ import prismadb from "@/lib/prismadb";
 import Navbar from "@/components/navbar";
 
 
-export default async function DashboardLayout({
+const DashboardLayout = async ({
   children, 
   params
 }: {
   children : React.ReactNode;
   params: { storeId: string }
-}) {
+}) => {
   const { userId } = auth();
   
   if(!userId) {
@@ -35,3 +35,5 @@ export default async function DashboardLayout({
     </>
   )
 };
+
+export default DashboardLayout;
